6283cabf09512a1be01ec903b3d2f9218b477397
mayakurella <112613673+mayakurella@users.noreply.github.com>
2024-03-08 16:32:37 +0100

    08.03-try1

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,19 +63,147 @@ $(document).ready(function () {
             }
         })
     })
-    document.addEventListener('DOMContentLoaded', function() {
-      var playText = document.getElementById('playAudioText');
-      var audioPlayer = document.getElementById('audioPlayer');
-  
-      playText.addEventListener('click', function() {
-          audioPlayer.play();
-      });
-  });
+    document.addEventListener('DOMContentLoaded', function () {
+        var playText = document.getElementById('playAudioText')
+        var audioPlayer = document.getElementById('audioPlayer')
 
-  document.getElementById('playAudio').addEventListener('click', function() {
-    var audio = document.getElementById('mayaAudio');
-    audio.play();
-  });
+        playText.addEventListener('click', function () {
+            audioPlayer.play()
+        })
+    })
+    //play im maya audio
+    document.getElementById('playAudio').addEventListener('click', function () {
+        var audio = document.getElementById('mayaAudio')
+        audio.play()
+    })
 
+    //play slowy
+    document
+        .getElementById('playAudio2')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('slowyAudio')
+            audio.play()
+        })
+
+    //play envelopes
+    document
+        .getElementById('playAudio3')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('envelopesAudio')
+            audio.play()
+        })
+
+    //play thoughts
+    document
+        .getElementById('playAudio4')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('thoughtsAudio')
+            audio.play()
+        })
+
+    //play billy
+    document
+        .getElementById('playAudio5')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('billyAudio')
+            audio.play()
+        })
+
+    //play beautiful
+    document
+        .getElementById('playAudio6')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('beautifulAudio')
+            audio.play()
+        })
+
+    //play analyse
+    document
+        .getElementById('playAudio7')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('analyseAudio')
+            audio.play()
+        })
+
+    //play forest bunny
+    document
+        .getElementById('playAudio8')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('bunnyAudio')
+            audio.play()
+        })
+
+    //play nature sky
+    document
+        .getElementById('playAudio9')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('natureAudio')
+            audio.play()
+        })
+
+    //play shrink
+    document
+        .getElementById('playAudio10')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('shrinkAudio')
+            audio.play()
+        })
+
+    //play bomb
+    document
+        .getElementById('playAudio11')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('bombAudio')
+            audio.play()
+        })
+
+    //play singing
+    document
+        .getElementById('playAudio12')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('singingAudio')
+            audio.play()
+        })
+
+    //play inside
+    document
+        .getElementById('playAudio13')
+        .addEventListener('click', function () {
+            var audio = document.getElementById('insideAudio')
+            audio.play()
+        })
+
+ //play dumb
+    document
+    .getElementById('playAudio14')
+    .addEventListener('click', function () {
+        var audio = document.getElementById('dumbAudio')
+        audio.play()
+    })
+
+ //play zubazuba
+ document
+ .getElementById('playAudio15')
+ .addEventListener('click', function () {
+     var audio = document.getElementById('zubazubaAudio')
+     audio.play()
+ })
+
+  //play what did
+  document
+  .getElementById('playAudio16')
+  .addEventListener('click', function () {
+      var audio = document.getElementById('whatAudio')
+      audio.play()
   })
 
+  document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggleButton');
+    const dropdownContent = document.getElementById('dropdownContent');
+  
+    toggleButton.addEventListener('click', function() {
+      dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+  
+})
